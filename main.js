59fe8e9090bb426c1dbbce3993159958fa7a279e
vanessa330 <vanessa.33010@gmail.com>
2022-11-13 12:22:27 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// https://www.youtube.com/watch?v=Efo7nIUF2JY
+
+const notesContainer = document.getElementById("app");
+const addNoteButton = document.querySelector(".add-note");
+
+function getNotes() {
+    return JSON.parse(localStorage.getItem("stick"))
+};
+
+function saveNotes(notes) {
+
+};
+
+function createNoteElement(id, content) {
+
+};
+
+function addNote() {
+
+};
+
+function updateNote(id, newContent) {
+
+};
+
+function deleteNote(id, element) {
+
+}
